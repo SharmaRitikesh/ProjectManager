66bf43c16ed00e6014e1759c69aa1a79a3a2ccaf
Ritikesh Sharma <ritikeshrajgoh123@gmail.com>
2026-03-16 23:48:37 +0530

    fix: remove node_modules from git, add vercel.json, fix mvnw.cmd for paths with special chars

--- backend/mvnw.cmd
+++ backend/mvnw.cmd
@@ -1,41 +1,42 @@
 @REM ----------------------------------------------------------------------------
 @REM Maven Start Up Batch script
+@REM Fixed to handle paths with spaces and special characters like +
 @REM ----------------------------------------------------------------------------
 
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
+set "WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain"
 
-@REM Find project base dir
-set MAVEN_PROJECTBASEDIR=%~dp0
+@REM Find project base dir using short (8.3) path to avoid special char issues
+for %%I in ("%~dp0.") do set "MAVEN_PROJECTBASEDIR=%%~sI\"
 
 @REM Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
+set "JAVA_EXE=java.exe"
+"%JAVA_EXE%" -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto execute
 
 echo Error: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 goto end
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set "JAVA_HOME=%JAVA_HOME:"=%"
+set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
 if exist "%JAVA_EXE%" goto execute
 echo Error: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 goto end
 
 :execute
-set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%.mvn\wrapper\maven-wrapper.jar"
+set "WRAPPER_JAR=%MAVEN_PROJECTBASEDIR%.mvn\wrapper\maven-wrapper.jar"
 
-if exist %WRAPPER_JAR% (
-    "%JAVA_EXE%" -cp %WRAPPER_JAR% %WRAPPER_LAUNCHER% %*
+if exist "%WRAPPER_JAR%" (
+    "%JAVA_EXE%" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" -cp "%WRAPPER_JAR%" %WRAPPER_LAUNCHER% %*
 ) else (
     echo Maven wrapper jar not found. Downloading...
     powershell -Command "Invoke-WebRequest -Uri 'https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.2.0/maven-wrapper-3.2.0.jar' -OutFile '%MAVEN_PROJECTBASEDIR%.mvn\wrapper\maven-wrapper.jar'"
-    "%JAVA_EXE%" -cp %WRAPPER_JAR% %WRAPPER_LAUNCHER% %*
+    "%JAVA_EXE%" "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" -cp "%WRAPPER_JAR%" %WRAPPER_LAUNCHER% %*
 )
 
 :end
